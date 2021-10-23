040397b5ea326aa1696b7bcbd4a21eee0692076d
J K Terry <justinkterry@gmail.com>
2021-10-23 11:58:20 -0400

    projections

--- plot.py
+++ plot.py
@@ -11,36 +11,34 @@ model = PPO.load("ppo")
 print('Starting loops')
 
 for env_seed in [1, 2, 3]:
-    for model_seed in [4, 5, 6]:
+    env.seed(env_seed)
 
-        model.set_random_seed(model_seed)
-        env.seed(env_seed)
+    obs_list = []
 
-        obs_list = []
-
-        obs = env.reset()
+    obs = env.reset()
+    obs_list.append(obs)
+    while True:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = env.step(action)
         obs_list.append(obs)
-        while True:
-            action, _states = model.predict(obs, deterministic=True)
-            obs, reward, done, info = env.step(action)
-            obs_list.append(obs)
-            if done:
-                break
+        if done:
+            break
+
+    for i in [0, 1, 2, 3]:
 
         xs = []
         ys = []
         zs = []
 
         for obs in obs_list:
-            xs.append(obs[0])
-            ys.append(obs[1])
-            zs.append(obs[2])
+            xs.append(obs[0+i])
+            ys.append(obs[1+i])
+            zs.append(obs[2+i])
 
         ax = plt.gca(projection="3d")
         ax.scatter(xs, ys, zs, c='r')
         ax.plot(xs, ys, zs, color='r')
 
         print('Loop saving')
-        print(len(obs_list))
 
-        plt.savefig('./plots/' + str(env_seed) + '_' + str(model_seed) + '.png')
+        plt.savefig('./plots/' + str(env_seed) + '_' + str(i) + '.png')
