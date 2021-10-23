e86232b3133cc6465e58d0ed6aa3d0dcc1b837c5
J K Terry <justinkterry@gmail.com>
2021-10-23 07:08:53 -0400

    seperate rendering

--- learn.py
+++ learn.py
@@ -10,39 +10,3 @@ env = make_vec_env("Acrobot2-v0", n_envs=4)
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=125000)
 model.save("ppo")
-
-del model  # remove to demonstrate saving and loading
-
-model = PPO.load("ppo")
-
-model.set_random_seed(1)
-env.seed(1)
-
-obs_list = []
-
-obs = env.reset()
-obs_list.append(obs)
-while True:
-    action, _states = model.predict(obs, determinstic=True)
-    obs, rewards, dones, info = env.step(action)
-    obs_list.append(obs)
-
-
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
-xs = []
-ys = []
-zs = []
-
-
-for obs in obs_list:
-    xs.append(obs[0])
-    ys.append(obs[0])
-    zs.append(obs[0])
-
-ax = plt.gca(projection="3d")
-ax.scatter(xs, ys, zs, c='r', s=100)
-ax.plot(xs, ys, zs, color='r')
-
-plt.savefig('sweet_potatoes.png')
---
+++ plot.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from stable_baselines3 import PPO
+import gym
+
+env = gym.make("Acrobot2-v0")
+
+model = PPO.load("ppo")
+
+model.set_random_seed(1)
+env.seed(1)
+
+obs_list = []
+
+obs = env.reset()
+obs_list.append(obs)
+while True:
+    action, _states = model.predict(obs, determinstic=True)
+    obs, rewards, dones, info = env.step(action)
+    obs_list.append(obs)
+
+xs = []
+ys = []
+zs = []
+
+
+for obs in obs_list:
+    xs.append(obs[0])
+    ys.append(obs[0])
+    zs.append(obs[0])
+
+ax = plt.gca(projection="3d")
+ax.scatter(xs, ys, zs, c='r', s=100)
+ax.plot(xs, ys, zs, color='r')
+
+plt.savefig('sweet_potatoes.png')
