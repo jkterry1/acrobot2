66e12052321d3b9095c50d4e6cf04c0924afd3c2
J K Terry <justinkterry@gmail.com>
2021-10-23 06:26:29 -0400

    acrobot

--- learn.py
+++ learn.py
@@ -1,18 +1,19 @@
 import gym
+import acrobot2
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 # Parallel environments
-env = make_vec_env("Acrobot-v1", n_envs=4)
+env = make_vec_env("Acrobot2", n_envs=4)
 
 model = PPO("MlpPolicy", env, verbose=1)
 model.learn(total_timesteps=25000)
-model.save("ppo_cartpole")
+model.save("ppo")
 
-del model # remove to demonstrate saving and loading
+del model  # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_cartpole")
+model = PPO.load("ppo")
 
 obs = env.reset()
 while True:
